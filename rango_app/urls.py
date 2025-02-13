f0c0bd1e843c71e0ca6c64ec528847949fbf9eec
Zeyu Wang <2752282W@student.gla.ac.uk>
2025-02-13 19:38:19 +0000

    Configured templates and updated views to render templates

--- rango_app/urls.py
+++ rango_app/urls.py
@@ -3,4 +3,7 @@ from rango_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:page_id>/', views.detail, name='detail'),
+    path('<int:category_id>/results/', views.results, name='results'),
+    path('<int:page_id>/vote/', views.vote, name='vote'),
 ]
--- rango_app/views.py
+++ rango_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def detail(request, page_id):
+    return render(request, 'detail.html', {'page_id': page_id})
+
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse( "Wellcome to Rango")
+def results(request, category_id): 
+    return HttpResponse(f"You are viewing results for category {category_id}.")
+
+def vote(request, page_id): 
+    return HttpResponse(f"You are voting on page {page_id}.")
