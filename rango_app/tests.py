cd7f3002796801668a7f42012151b7cf6d2f65b8
Zeyu Wang <2752282W@student.gla.ac.uk>
2025-02-13 20:39:46 +0000

    Added unit tests for models and views

--- rango_app/tests.py
+++ rango_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from rango_app.models import Category
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+        category = Category.objects.create(name='Test Category')
+        self.assertEqual(category.name, 'Test Category')
+
+class ViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome to Rango!')
+
+    def test_add_category_view(self):
+        response = self.client.get(reverse('add_category'))
+        self.assertEqual(response.status_code, 200)
