d4f88c0069058fa05462847ba4bea55bfafb330f
Zeyu Wang <2752282W@student.gla.ac.uk>
2025-02-13 20:17:25 +0000

    Customized Django Admin interface for Category and Page

--- rango_app/admin.py
+++ rango_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from rango_app.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'views')
+    list_filter = ('category',)
+    search_fields = ('title', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'name': ('name',)}
+    list_display = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
